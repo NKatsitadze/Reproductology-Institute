c626bf6ef6eaad25407abedf15294f75ad123371
nika katsitadze <nikakacitadze98@gmail.com>
2023-01-19 11:45:46 -0800

    dist

---
+++ dist/assets/TeamPage-14777de7.js
@@ -0,0 +1,152 @@
+import{u as g,I as r,b as I,c as l,d as p,e as c,f as d,g as P,h as T,i as $,k as h,l as x,m as y,n as b,o as u,p as w,q as v,r as G,t as M,v as s,j as a,a as m,s as N}from"./index-15372c02.js";const k=function(){const{t:e}=g(),t=[{id:0,imgPath:r,name:`${e("TeamPage.team.staff0.name")}`,lastname:`${e("TeamPage.team.staff0.lastname")}`,shortInfo:`${e("TeamPage.team.staff0.shortInfo")}`,longInfo:`${e("TeamPage.team.staff0.longInfo")}`,experienceInfo:`${e("TeamPage.team.staff0.experienceInfo")}`,pedagogyInfo:"",participentInfo:`${e("TeamPage.team.staff0.participentInfo")}`},{id:1,imgPath:I,name:`${e("TeamPage.team.staff1.name")}`,lastname:`${e("TeamPage.team.staff1.lastname")}`,shortInfo:`${e("TeamPage.team.staff1.shortInfo")}`,longInfo:`${e("TeamPage.team.staff1.longInfo")}`,experienceInfo:`${e("TeamPage.team.staff1.experienceInfo")}`,pedagogyInfo:`${e("TeamPage.team.staff1.pedagogyInfo")}`,participentInfo:`${e("TeamPage.team.staff1.participentInfo")}`},{id:2,imgPath:l,name:`${e("TeamPage.team.staff2.name")}`,lastname:`${e("TeamPage.team.staff2.lastname")}`,shortInfo:`${e("TeamPage.team.staff2.shortInfo")}`,longInfo:`${e("TeamPage.team.staff2.longInfo")}`,experienceInfo:`${e("TeamPage.team.staff2.experienceInfo")}`,pedagogyInfo:`${e("TeamPage.team.staff2.pedagogyInfo")}`,participentInfo:`${e("TeamPage.team.staff2.participentInfo")}`},{id:3,imgPath:p,name:`${e("TeamPage.team.staff3.name")}`,lastname:`${e("TeamPage.team.staff3.lastname")}`,shortInfo:`${e("TeamPage.team.staff3.shortInfo")}`,longInfo:`${e("TeamPage.team.staff3.longInfo")}`,experienceInfo:`${e("TeamPage.team.staff3.experienceInfo")}`,pedagogyInfo:`${e("TeamPage.team.staff3.pedagogyInfo")}`,participentInfo:""},{id:4,imgPath:c,name:`${e("TeamPage.team.staff4.name")}`,lastname:`${e("TeamPage.team.staff4.lastname")}`,shortInfo:`${e("TeamPage.team.staff4.shortInfo")}`,longInfo:`${e("TeamPage.team.staff4.longInfo")}`,experienceInfo:`${e("TeamPage.team.staff4.experienceInfo")}`,pedagogyInfo:"",participentInfo:""},{id:5,imgPath:d,name:`${e("TeamPage.team.staff5.name")}`,lastname:`${e("TeamPage.team.staff5.lastname")}`,shortInfo:`${e("TeamPage.team.staff5.shortInfo")}`,longInfo:`${e("TeamPage.team.staff5.longInfo")}`,experienceInfo:`${e("TeamPage.team.staff5.experienceInfo")}`,pedagogyInfo:"",participentInfo:""},{id:6,imgPath:P,name:`${e("TeamPage.team.staff6.name")}`,lastname:`${e("TeamPage.team.staff6.lastname")}`,shortInfo:`${e("TeamPage.team.staff6.shortInfo")}`,longInfo:`${e("TeamPage.team.staff6.longInfo")}`,experienceInfo:`${e("TeamPage.team.staff6.experienceInfo")}`,pedagogyInfo:"",participentInfo:""},{id:7,imgPath:T,name:`${e("TeamPage.team.staff7.name")}`,lastname:`${e("TeamPage.team.staff7.lastname")}`,shortInfo:`${e("TeamPage.team.staff7.shortInfo")}`,longInfo:"",experienceInfo:"",pedagogyInfo:"",participentInfo:""},{id:8,imgPath:$,name:`${e("TeamPage.team.staff8.name")}`,lastname:`${e("TeamPage.team.staff8.lastname")}`,shortInfo:`${e("TeamPage.team.staff8.shortInfo")}`,longInfo:`${e("TeamPage.team.staff8.longInfo")}`,experienceInfo:`${e("TeamPage.team.staff8.experienceInfo")}`,pedagogyInfo:`${e("TeamPage.team.staff8.pedagogyInfo")}`,participentInfo:`${e("TeamPage.team.staff8.participentInfo")}`},{id:9,imgPath:h,name:`${e("TeamPage.team.staff9.name")}`,lastname:`${e("TeamPage.team.staff9.lastname")}`,shortInfo:`${e("TeamPage.team.staff9.shortInfo")}`,longInfo:`${e("TeamPage.team.staff9.longInfo")}`,experienceInfo:`${e("TeamPage.team.staff9.experienceInfo")}`,pedagogyInfo:`${e("TeamPage.team.staff9.pedagogyInfo")}`,participentInfo:""},{id:10,imgPath:x,name:`${e("TeamPage.team.staff10.name")}`,lastname:`${e("TeamPage.team.staff10.lastname")}`,shortInfo:`${e("TeamPage.team.staff10.shortInfo")}`,longInfo:`${e("TeamPage.team.staff10.longInfo")}`,experienceInfo:`${e("TeamPage.team.staff10.experienceInfo")}`,pedagogyInfo:`${e("TeamPage.team.staff10.pedagogyInfo")}`,participentInfo:""},{id:11,imgPath:y,name:`${e("TeamPage.team.staff11.name")}`,lastname:`${e("TeamPage.team.staff11.lastname")}`,shortInfo:`${e("TeamPage.team.staff11.shortInfo")}`,longInfo:`${e("TeamPage.team.staff11.longInfo")}`,experienceInfo:"",pedagogyInfo:"",participentInfo:""},{id:12,imgPath:b,name:`${e("TeamPage.team.staff12.name")}`,lastname:`${e("TeamPage.team.staff12.lastname")}`,shortInfo:`${e("TeamPage.team.staff12.shortInfo")}`,longInfo:`${e("TeamPage.team.staff12.longInfo")}`,experienceInfo:"",pedagogyInfo:"",participentInfo:""},{id:13,imgPath:u,name:`${e("TeamPage.team.staff13.name")}`,lastname:`${e("TeamPage.team.staff13.lastname")}`,shortInfo:`${e("TeamPage.team.staff13.shortInfo")}`,longInfo:`${e("TeamPage.team.staff13.longInfo")}`,experienceInfo:`${e("TeamPage.team.staff13.experienceInfo")}`,pedagogyInfo:`${e("TeamPage.team.staff13.pedagogyInfo")}`,participentInfo:`${e("TeamPage.team.staff13.participentInfo")}`},{id:14,imgPath:w,name:`${e("TeamPage.team.staff14.name")}`,lastname:`${e("TeamPage.team.staff14.lastname")}`,shortInfo:`${e("TeamPage.team.staff14.shortInfo")}`,longInfo:`${e("TeamPage.team.staff14.longInfo")}`,experienceInfo:`${e("TeamPage.team.staff14.experienceInfo")}`,pedagogyInfo:`${e("TeamPage.team.staff14.pedagogyInfo")}`,participentInfo:""},{id:15,imgPath:v,name:`${e("TeamPage.team.staff15.name")}`,lastname:`${e("TeamPage.team.staff15.lastname")}`,shortInfo:`${e("TeamPage.team.staff15.shortInfo")}`,longInfo:`${e("TeamPage.team.staff15.longInfo")}`,experienceInfo:`${e("TeamPage.team.staff15.experienceInfo")}`,pedagogyInfo:"",participentInfo:""},{id:16,imgPath:G,name:`${e("TeamPage.team.staff16.name")}`,lastname:`${e("TeamPage.team.staff16.lastname")}`,shortInfo:`${e("TeamPage.team.staff16.shortInfo")}`,longInfo:`${e("TeamPage.team.staff16.longInfo")}`,experienceInfo:`${e("TeamPage.team.staff16.experienceInfo")}`,pedagogyInfo:`${e("TeamPage.team.staff16.pedagogyInfo")}`,participentInfo:`${e("TeamPage.team.staff16.participentInfo")}`},{id:17,imgPath:M,name:`${e("TeamPage.team.staff17.name")}`,lastname:`${e("TeamPage.team.staff17.lastname")}`,shortInfo:`${e("TeamPage.team.staff17.shortInfo")}`,longInfo:`${e("TeamPage.team.staff17.longInfo")}`,experienceInfo:`${e("TeamPage.team.staff17.experienceInfo")}`,pedagogyInfo:"",participentInfo:""}],[n,i]=s.exports.useState(0),o=s.exports.useRef(null);return a(j,{children:m("div",{className:"center-container",children:[a("div",{className:"image-container",children:t.map(f=>a("div",{onClick:()=>{o.current.scrollIntoView(),i(f.id)},className:"image-box",children:a("img",{className:"doc-img",src:f.imgPath,alt:"doctor photo"})},f.id))}),a("div",{className:"scroll-box",children:a("div",{ref:o,className:"scroll-fix"})}),m("div",{className:"member-box",children:[a("div",{className:"img-box",children:a("img",{className:"img",src:t[n].imgPath,alt:""})}),m("div",{className:"info-list",children:[m("p",{className:"name",children:[t[n].name," ",t[n].lastname]}),a("p",{children:t[n].shortInfo}),a("p",{children:t[n].longInfo}),a("p",{children:t[n].experienceInfo}),a("p",{children:t[n].participentInfo})]})]})]})})},j=N.section`
+    margin-top: 5.8rem;
+    padding: 0.1rem 0;
+    width: 100%;
+
+    .center-container {
+        width: 90%;
+        margin: 0 auto;
+
+        .image-container {
+            display: grid;
+            grid-template-columns: repeat(6, 1fr);
+            grid-row-gap: 0.15rem;
+            grid-column-gap: 0.2rem;
+            overflow: hidden;
+            width: 100%;
+            overflow: hidden;
+
+            .image-box {
+                overflow: hidden;
+                width: 100%;
+                height: 17rem;
+                border: 1px solid var(--Hover-Lightblue);
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                background: var(--Hover-Lightblue);
+                cursor: pointer;
+
+                &:hover .doc-img {
+                    transform: scale(1.1);
+                    filter: brightness(1.1);
+                }
+
+                .doc-img {
+                    width: 100%;
+                    height: 100%;
+                    transform: scale(1.05);
+                    object-fit: cover; 
+                    transition: all 0.2s;  
+                    object-position: top;                 
+                }
+            }
+        }
+
+        .scroll-box {
+            position: relative;
+            .scroll-fix {
+                position: absolute;
+                bottom: 0%;
+                opacity: 0;
+                height: 2.8rem;
+                width: 4rem;
+                pointer-events: none;
+            }
+        }
+
+        .member-box {
+            margin-top: 2rem;
+            background: var(--Hover-Lightblue);
+            display: flex;
+            border-radius: 0.3rem;
+            overflow: hidden;
+           
+            .img-box {
+                
+                .img {
+                    width: 15rem;
+                    object-fit: cover;
+                }
+            }
+
+            .info-list {
+                padding: 2rem 1rem;
+                display: flex;
+                flex-direction: column;
+                gap: 1rem;
+                font-size: 1.05rem;
+
+                .name {
+                    font-weight: 700;
+                    font-size: 1.3rem;
+                }
+            }
+        }
+    }
+
+        //    Below 1440px
+           @media(max-width: 90em) {
+                .center-container {
+                    width: 100%;
+                }
+           }
+                    // Below 1150px
+                    @media(max-width: 71.875em) {
+                        .center-container {
+                            .image-container {
+                                grid-template-columns: repeat(5,1fr);
+                            }
+
+                            .member-box {
+                                .img-box {
+                                    display: none;
+                                }
+
+                                .info-list {
+                                    padding: 2rem 2.5rem;
+                                    .name {
+                                        text-align: center;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                            // Below 950px
+                            @media(max-width: 59.375em) {
+                                .center-container {
+                                    .image-container {
+                                        grid-template-columns: repeat(4, 1fr);
+                                    }
+                                }
+                            }
+                                        // Below 750px
+                                        @media(max-width: 46.875em) {
+                                            .center-container {
+                                                .image-container {
+                                                    grid-template-columns: repeat(3, 1fr);
+                                                    grid-row-gap: 0.1rem;
+                                                    grid-column-gap: 0.1rem;
+                                                }
+                                            }
+                                        }
+                                                // Below 550px
+                                                @media(max-width: 34.375em) {
+                                                    .center-container {
+                                                        .image-container {
+                                                            grid-template-columns: repeat(2, 1fr);
+                                                        }
+
+                                                        .member-box {
+                                                            .info-list {
+                                                                padding: 1rem 0.5rem;
+                                                            }
+                                                        }
+                                                    }
+                                                }
+
+                                                        // Below 425px
+                                                        @media(max-width: 26.5625em) {
+                                                            margin-top: 4.6rem;
+                                                        }
+`;export{k as default};
